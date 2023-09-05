3fbc3fd2e0729741e0ea89abf1ed3c0eff58599b
Sam Malayek <malayek@gmail.com>
2023-09-04 19:15:48 -0700

    Add day 20

---
+++ day20-part1.py
@@ -0,0 +1,27 @@
+
+def main():
+    raw = open('day20.txt', 'r').read().splitlines()
+
+    ips = []
+
+    for line in raw:
+        ips.append(list(map(int, line.split('-'))))
+
+    ips.sort()
+
+    i = 0
+    while i < len(ips)-1:
+        if ips[i][1] >= ips[i+1][0]-1:
+            ips[i][1] = max(ips[i][1], ips[i+1][1])
+            ips.pop(i+1)
+            continue
+        i += 1
+
+    if ips[0][0] == 0:
+        print(ips[0][1]+1)
+    else:
+        print(ips[0][0]-1)
+
+
+if __name__ == "__main__":
+    main()
---
+++ day20-part2.py
@@ -0,0 +1,33 @@
+
+def main():
+    raw = open('day20.txt', 'r').read().splitlines()
+
+    ips = []
+
+    for line in raw:
+        ips.append(list(map(int, line.split('-'))))
+
+    ips.sort()
+
+    i = 0
+    while i < len(ips)-1:
+        if ips[i][1] >= ips[i+1][0]-1:
+            ips[i][1] = max(ips[i][1], ips[i+1][1])
+            ips.pop(i+1)
+            continue
+        i += 1
+
+    i = 0
+    resp = (4294967295 - ips[-1][1])
+    while i < len(ips) - 1:
+        cur = ips[i]
+        nex = ips[i+1]
+
+        resp += (nex[0] - cur[1] - 1)
+        i += 1
+
+    print(resp)
+
+
+if __name__ == "__main__":
+    main()
