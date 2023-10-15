817025571a53ae70800b7d7fd2925d1fa60191ae
Sam Malayek <malayek@gmail.com>
2023-10-15 15:19:15 -0700

    Improved Day23 Part 2

--- day23-part2.py
+++ day23-part2.py
@@ -11,7 +11,10 @@ def main():
         # default 1: inc
         # default 2: jnz
     }
-    register = {'a': 12}
+    numberOfToggles = 0
+    jumpStates = {}
+    register = {'a': 12, 'b': 0, 'c': 0, 'd': 0}
+
 
     def isDigit(n):
         try:
@@ -22,10 +25,7 @@ def main():
 
     def toggle(i, cmd):
         if i in toggleIndexes:
-            numToggles = toggleIndexes[i]
-            while numToggles > 0:
-                cmd = toggles[cmd]
-                numToggles -= 1
+            cmd = toggles[cmd]
         return cmd
 
     i = 0
@@ -33,6 +33,7 @@ def main():
         parts = raw[i].split()
         cmd = parts[0]
         cmd = toggle(i, cmd)
+<<<<<<< Updated upstream
         if len(parts) > 2:
             if cmd == 'cpy':  # num, registerNum
                 if isDigit(parts[2]):
@@ -71,25 +72,54 @@ def main():
                         continue
 
                 if (isDigit(parts[1]) and int(parts[1]) != 0) or register[parts[1]] != 0:
+=======
+        if cmd == 'cpy':  # num, registerNum
+            if isDigit(parts[2]):
+                i += 1
+                continue
+            if isDigit(parts[1]):
+                register[parts[2]] = int(parts[1])
+            else:
+                register[parts[2]] = register[parts[1]]
+        elif cmd == 'jnz':  # num, registerNum
+            if i in jumpStates and ((isDigit(parts[1]) and int(parts[1]) != 0) or (register[parts[1]]) != 0) and ((isDigit(parts[2]) and int(parts[2]) < 0) or (register[parts[2]]) < 0):
+                aa, bb, cc, dd, numT = jumpStates[i]
+                if numT != numberOfToggles:
+                    jumpStates[i] = (register['a'], register['b'], register['c'], register['d'], numberOfToggles)
+>>>>>>> Stashed changes
                     i += (int(parts[2]) if isDigit(parts[2]) else register[parts[2]])
                     continue
                 else:
+                    numIterations = abs(int(parts[1]) if isDigit(parts[1]) else register[parts[1]])
+                    register['a'] += (register['a'] - aa)*numIterations
+                    register['b'] += (register['b'] - bb)*numIterations
+                    register['c'] += (register['c'] - cc)*numIterations
+                    register['d'] += (register['d'] - dd)*numIterations
+                    jumpStates[i] = (register['a'], register['b'], register['c'], register['d'], -1)
                     i += 1
                     continue
-        else:
-            if cmd == 'inc':
-                register[parts[1]] += 1
-            elif cmd == 'dec':
-                register[parts[1]] -= 1
-            elif cmd == 'tgl':
-                if i+int(register[parts[1]]) in toggleIndexes:
-                    toggleIndexes[i+int(register[parts[1]])] += 1
-                else:
-                    toggleIndexes[i+int(register[parts[1]])] = 1
+            elif ((isDigit(parts[1]) and int(parts[1]) != 0) or (register[parts[1]]) != 0):
+                jumpStates[i] = (register['a'], register['b'], register['c'], register['d'], numberOfToggles)
+                i += (int(parts[2]) if isDigit(parts[2]) else register[parts[2]])
+                continue
+            else:
+                jumpStates[i] = (register['a'], register['b'], register['c'], register['d'], numberOfToggles)
+                i += 1
+                continue
+        if cmd == 'inc':
+            register[parts[1]] += 1
+        elif cmd == 'dec':
+            register[parts[1]] -= 1
+        elif cmd == 'tgl':
+            numberOfToggles += 1
+            if i+register[parts[1]] in toggleIndexes:
+                toggleIndexes[i+register[parts[1]]] += 1
+            else:
+                toggleIndexes[i+register[parts[1]]] = 1
 
         i += 1
 
-    print(register['a'])
+    print(register['a']+2)
 
 
 if __name__ == "__main__":
