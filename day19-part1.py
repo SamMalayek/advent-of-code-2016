966524cf6e0d22269fd81d012150b726b417893d
Sam Malayek <malayek@gmail.com>
2023-12-10 17:55:41 -0800

    Update day19 part 1 after watching Numberphile

--- day19-part1.py
+++ day19-part1.py
@@ -2,26 +2,13 @@
 def main():
     numElves = int(open('day19.txt', 'r').read().rstrip())
 
-    elvesToNumPresents = [1 for _ in range(numElves)]
+    # Explanation for the magic below on Numberphile: https://www.youtube.com/watch?v=uCsD3ZGzMgE
 
-    maxPresentsForOneElf = 1
+    numElvesBin = bin(numElves)[2:]
 
-    curElf = 0
-    while maxPresentsForOneElf < numElves:
-        if elvesToNumPresents[curElf] > 0:
-            nextElf = (curElf + 1) % numElves
-            while elvesToNumPresents[nextElf] == 0:
-                nextElf = (nextElf + 1) % numElves
-            elvesToNumPresents[curElf] += elvesToNumPresents[nextElf]
-            elvesToNumPresents[nextElf] = 0
+    winningPosBin = numElvesBin[1:] + numElvesBin[0]
 
-            maxPresentsForOneElf = max(maxPresentsForOneElf, elvesToNumPresents[curElf])
-            if maxPresentsForOneElf != numElves:
-                curElf = nextElf
-        else:
-            curElf = (curElf + 1) % numElves
-
-    print(curElf+1)
+    print(int(winningPosBin, 2))
 
 
 if __name__ == "__main__":
